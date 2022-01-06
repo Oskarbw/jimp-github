6bf879b9cf268fe705dcaab5f53817be9e9ccd77
Oskarbw <97109358+Oskarbw@users.noreply.github.com>
2022-01-06 18:26:58 +0100

    Add files via upload
    Dodano wybór elementu głównego w kolumnach

--- src/gauss.c
+++ src/gauss.c
@@ -1,16 +1,48 @@
 #include "gauss.h"
+#include <math.h>
+
+void zamien(double** a, double** b)
+{
+	double* c = *a;
+	*a = *b;
+	*b = c;
+}
+
+void wyborElementu (Matrix* mat, Matrix* b, int i)
+{
+	double max = 0;
+	int nrMax = 0;
+	int nrWier = i;
+	for (i;i<mat->r;i++)
+	{
+		if(fabs(mat->data[i][nrWier])>max)
+		{
+			nrMax = i;
+			max = fabs(mat->data[i][nrWier]);
+		}
+	}
+
+	zamien(&mat->data[nrMax], &mat->data[nrWier]);
+	zamien(&b->data[nrMax], &b->data[nrWier]);
+}
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 Mock of the implementation ...
-		 */
-		
-
-		return 0;
+	for (int i=0; i < mat->c && i < mat->r; i++){
+		wyborElementu(mat,b,i);
+		if (mat->data[i][i] == 0.0) /* macierz osobliwa, dzielenie przez 0 */
+			return 1;
+		for (int j=i+1; j < mat->r; j++){
+			double dziel = mat->data[j][i] / mat->data[i][i];
+			for (int k=0; k < mat->c; k++){
+				mat->data[j][k] -= dziel * mat->data[i][k];
+			}
+			b->data[j][0] -= dziel * b->data[i][0];
+		}
+	}
+	return 0;
 }
 
